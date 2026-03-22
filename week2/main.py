ce0413ccdc10029487ccd2cbc87315bd546ee911
hiralsinghal <singhal.hiral@gmail.com>
2026-03-23 01:38:41 +0530

    started week2

---
+++ week2/main.py
@@ -0,0 +1,28 @@
+import argparse
+import requests
+import sys
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest="command")
+
+greet_parser = subparsers.add_parser("greet", help="Greet someone")
+greet_parser.add_argument("name", type=str, help="Name of the person to greet")
+
+pokemon_parser = subparsers.add_parser("pokemon", help="Favourite Pokemon")
+pokemon_parser.add_argument("fav_pokemon",type=str, help="Your favourite Pokemon")
+
+args = parser.parse_args()
+
+if args.command == "greet":
+    print(f"Hello, {args.name}!")
+
+if args.command == "pokemon":
+    print(f"Your favourite pokemon is {args.fav_pokemon}")
+
+req = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
+if req.status_code != 200:
+    print("API returned with error")
+    sys.exit(1)
+api_parsed = req.json()
+print(f"Ablility 1 is {api_parsed["abilities"][0]["ability"]["name"]}")
+print(f'Ability 1 being hidden is {api_parsed["abilities"][0]["is_hidden"]}') 
