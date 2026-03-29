3df480114f3775eb361980c9063df6ef75f22fb2
hiralsinghal <singhal.hiral@gmail.com>
2026-03-29 21:12:48 +0530

    working on week2

--- week2/main.py
+++ week2/main.py
@@ -8,8 +8,8 @@ subparsers = parser.add_subparsers(dest="command")
 greet_parser = subparsers.add_parser("greet", help="Greet someone")
 greet_parser.add_argument("name", type=str, help="Name of the person to greet")
 
-pokemon_parser = subparsers.add_parser("pokemon", help="Favourite Pokemon")
-pokemon_parser.add_argument("fav_pokemon",type=str, help="Your favourite Pokemon")
+pokemon_parser = subparsers.add_parser("pokemon", help="Search a Pokemon")
+pokemon_parser.add_argument("pokemon",type=str, help="Your favourite Pokemon")
 
 args = parser.parse_args()
 
@@ -17,12 +17,14 @@ if args.command == "greet":
     print(f"Hello, {args.name}!")
 
 if args.command == "pokemon":
-    print(f"Your favourite pokemon is {args.fav_pokemon}")
+    print(f"Your favourite pokemon is {args.pokemon}")
 
-req = requests.get("https://pokeapi.co/api/v2/pokemon/ditto")
-if req.status_code != 200:
-    print("API returned with error")
-    sys.exit(1)
-api_parsed = req.json()
-print(f"Ablility 1 is {api_parsed["abilities"][0]["ability"]["name"]}")
-print(f'Ability 1 being hidden is {api_parsed["abilities"][0]["is_hidden"]}') 
+if args.command=="pokemon":
+    req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{args.pokemon}")
+    if req.status_code != 200:
+        print("API returned with error")
+        sys.exit(1)
+    api_parsed = req.json()
+    print(f"args.pokemon")
+    print(f"Ablility 1 is {api_parsed['abilities'][0]['ability']['name']}")
+    print(f"Ability 1 being hidden is {api_parsed['abilities'][0]['is_hidden']}") 
